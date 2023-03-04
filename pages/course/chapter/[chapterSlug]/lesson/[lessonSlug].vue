2d1365c809d264a513fbef0f51bd3c56c26454d3
Mazaev Andrey <mazaev2000@gmail.com>
2023-03-04 14:14:52 +0300

    feat: update course information

--- pages/course/chapter/[chapterSlug]/lesson/[lessonSlug].vue
+++ pages/course/chapter/[chapterSlug]/lesson/[lessonSlug].vue
@@ -13,11 +13,38 @@ const lesson = computed(() => {
     (lesson) => lesson.slug === route.params.lessonSlug
   );
 });
+
+const title = computed(() => {
+  return `${lesson.value.title} - ${course.title}`;
+});
+useHead({
+  title,
+});
 </script>
 
 <template>
-  <h2>Lesson</h2>
-  <p>This is a lesson</p>
-  <p>{{ chapter.title }}</p>
-  <p>{{ lesson.title }}</p>
+  <div>
+    <p class="mt-0 uppercase font-bold text-slate-400 mb-1">
+      Lesson {{ chapter.number }} - {{ lesson.number }}
+    </p>
+    <h2 class="my-0">{{ lesson.title }}</h2>
+    <div class="flex space-x-4 mt-2 mb-8">
+      <NuxtLink
+        v-if="lesson.sourceUrl"
+        class="font-normal text-md text-gray-500"
+        :to="lesson.sourceUrl"
+      >
+        Download Source Code
+      </NuxtLink>
+      <NuxtLink
+        v-if="lesson.downloadUrl"
+        class="font-normal text-md text-gray-500"
+        :to="lesson.downloadUrl"
+      >
+        Download Video
+      </NuxtLink>
+    </div>
+    <VideoPlayer v-if="lesson.videoId" :videoId="lesson.videoId" />
+    <p>{{ lesson.text }}</p>
+  </div>
 </template>
