f3e252851df617bda10e687642ac45308bf99301
Mazaev Andrey <mazaev2000@gmail.com>
2023-03-04 16:32:36 +0300

    feat: course title

--- pages/course.vue
+++ pages/course.vue
@@ -1,4 +1,12 @@
 <template>
+  <div class="prose mb-12">
+    <h1>
+      <span class="font-medium">
+        Course:
+        <span class="font-bold">Mastering Nuxt 3</span>
+      </span>
+    </h1>
+  </div>
   <div class="flex flex-row justify-center flex-grow">
     <div
       class="prose mr-4 p-8 bg-white rounded-md min-w-[20ch] max-w-[30ch] flex flex-col"
@@ -15,12 +23,10 @@
           :key="lesson.slug"
           class="flex flex-row space-x-1 no-underline prose-sm font-normal py-1 px-4 -mx-4"
           :to="lesson.path"
-            :class="{
-              'text-blue-500':
-                lesson.path === $route.fullPath,
-              'text-gray-600':
-                lesson.path !== $route.fullPath,
-            }"
+          :class="{
+            'text-blue-500': lesson.path === $route.fullPath,
+            'text-gray-600': lesson.path !== $route.fullPath,
+          }"
         >
           <span class="text-gray-500">{{ index + 1 }}.</span>
           <span>{{ lesson.title }}</span>
