2c66a201605444f8b645ec0d59e56c1d919f5007
Mazaev Andrey <mazaev2000@gmail.com>
2023-04-15 15:19:01 +0300

    feat: auth

---
+++ pages/login.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="prose w-full max-w-2xl h-9">
+    <h1>Log in to {{ title }}</h1>
+    <button
+      @click="login"
+      class="bg-blue-500 text-white font-bold py-2 px-4 rounded"
+    >
+      Log in with Github
+    </button>
+  </div>
+</template>
+
+<script lang="ts" setup>
+const { title } = useCourse();
+const supabase = useSupabaseClient();
+
+const login = async () => {
+  const { error } = await supabase.auth.signInWithOAuth({
+    provider: "github",
+  });
+
+  if (error) {
+    console.error(error);
+  }
+};
+</script>
